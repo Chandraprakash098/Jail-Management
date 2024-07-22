51041c1eef21b5d823e235a1123834cf593f1825
user.name like:"Chandra Prakash <user.email like:"chandraprakash96386@gmail.com>
2024-07-22 11:24:36 +0530

    Update backend server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const path = require("path");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const prisonerRoutes = require("./routes/prisonerRoutes");
@@ -18,6 +19,15 @@ app.use("/api/auth", authRoutes);
 app.use("/api/prisoners", prisonerRoutes);
 app.use("/api/visitations", visitationRoutes);
 app.use("/api/incidents", incidentRoutes);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+// Catch-all handler to serve the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
